9093a2456f7419faec9b8e1b5a6b98c9bedd690a
yasarserkan1601@gmail.com <yasarserkan1601@gmail.com>
2025-04-29 23:38:15 +0300

    Fix _output_config keys

--- src/vocavoice/ui_vocavoice.py
+++ src/vocavoice/ui_vocavoice.py
@@ -97,8 +97,8 @@ def run_vocavoice(
 
 
         return {
-            "script_full_path": _output_config["script_full_path"],
-            "audio_full_path": _output_config["audio_full_path"]
+            "script_file_full_path": _output_config["script_file_full_path"],
+            "audio_file_full_path": _output_config["audio_file_full_path"]
         }
 
     except Exception as e:
